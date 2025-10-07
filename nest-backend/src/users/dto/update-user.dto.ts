972f5f629b178d2df2ae73c296910aa75e54a280
bk-ghauri <bkghauri@gmail.com>
2025-10-07 13:00:16 +0500

    Fix errors

--- nest-backend/src/users/dto/update-user.dto.ts
+++ nest-backend/src/users/dto/update-user.dto.ts
@@ -1,14 +1,14 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString, IsUrl, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
-  @ApiProperty()
+  @ApiPropertyOptional()
   @IsString()
   @MinLength(3)
-  displayName: string;
+  displayName?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsUrl()
-  avatarUrl?: string; // optional profile picture URL
+  avatarUrl?: string;
 }
